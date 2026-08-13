4b9bf2c9932a4fc67bbfdf9b1f564b74e2f7594b
kenny <kennykarosy@gmail.com>
2026-08-12 20:57:24 -0400

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+const first_name_key = "dev-first-name";
+function refresh_greeting(first_name) {
+  const msg = `I hope you're not a weed ${first_name}`;
+  document.getElementById("greeting").innerText = msg;
+}
+function set_first_name(elem) {
+  const first_name = elem.value;
+  if (first_name.length > 0) {
+    localStorage.setItem(first_name_key, first_name);
+    document.getElementById("first-name").value = "";
+    refresh_greeting(first_name);
+  }
+}
+function init() {
+  const first_name = localStorage.getItem(first_name_key);
+  console.log(first_name);
+  if (first_name && first_name.length > 0) {
+    refresh_greeting(first_name);
+  }
+}
+document.addEventListener("DOMContentLoaded", () => {
+  init();
+});
